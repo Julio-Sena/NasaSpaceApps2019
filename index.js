2f159f1860afbfc5d29c8e6be026957068a7dca8
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2019-10-20 07:03:09 -0300

    first trash

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-let world, angle = 0.0;
-let naveX = 150, naveY = window.innerHeight/1.5, navePeso = 10, naveAng = 0
+let world, worldAngle = 0.0
+let naveX = 150, naveY = window.innerHeight/1.5, naveAng = 0
+let trashX = window.innerWidth, trashY = 0, trashAngle = 0.0
+const velocidade = 10
 
 function preload() {
   world = loadImage(`./public/img/world.png`)
   nave = loadImage(`./public/img/nave.png`)
+  trash = loadImage(`./public/img/trash.png`)
+  trashY = random(0,window.innerHeight)
 }
 
 function setup() {
@@ -15,13 +19,14 @@ function draw() {
   imageMode(CENTER)
 
   renderWorld()
+  renderTrash()
   renderNave()
 }
 
 function renderWorld() {
   push()
   translate(width / 2, height + height/1.1)
-  rotate(angle -= 0.0002)
+  rotate(worldAngle -= 0.0002)
   image(world, 0, 0, 1700, 1700)
   pop()
 }
@@ -30,11 +35,15 @@ function renderNave() {
   push()
 
   if (keyIsDown(UP_ARROW) && naveY >= 0) {
-    naveY -= navePeso
+    naveY -= velocidade
   } else if (keyIsDown(DOWN_ARROW) && naveY <= height) {
-    naveY += navePeso
+    naveY += velocidade
   }
 
   image(nave, naveX, naveY, 150, 100)
   pop()
+}
+
+function renderTrash() {
+  image(trash, trashX--, trashY, 50, 50)
 }
