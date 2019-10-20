9da0a9ebd91c57ca5e6c5b662cc760d25344fbb8
OliveiraLuis <oliveira1eduardo2luis3@gmail.com>
2019-10-20 02:48:39 -0300

    Commit initial

---
+++ index.js
@@ -0,0 +1,36 @@
+let world, angle = 0.0;
+let navePos = 150, navePeso = 10
+
+function preload() {
+  world = loadImage(`./public/img/world.png`)
+  nave = loadImage(`./public/img/nave.png`)
+}
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight)
+}
+
+function draw() {
+  background(51)
+  imageMode(CENTER)
+
+  renderWorld()
+  renderNave()
+}
+
+function renderWorld() {
+  translate(width / 2, height + height/1.1)
+  rotate(angle -= 0.0002)
+  image(nave, navePos, height/1.5, 150, 100)
+  // image(world, 0, 0, 1700, 1700)
+}
+
+function renderNave() {
+  image(nave, navePos, height/1.5, 150, 100)
+
+  if (keyIsDown(LEFT_ARROW)) {
+    navePos -= navePeso
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    navePos += navePeso
+  }
+}
